67f31db24a956ed21d3cc4b3dd1a22fb0b63e226
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-10-09 20:57:04 -0400

    initial commit for RocHack tutorial -- Antonio Cardenas will be the main developer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RocHackTutorial::Application.config.secret_key_base = '4a223eeeb4d316b5fa7c61d3d2320e477a31db5c63667cbc02e3a6d9a3e5e6f5711cf209e9cd947bc69d2cd96cbba62efa5f6a68eedf4b23112afe87cb1a43e9'
